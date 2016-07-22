9c60a8f0ed161a6638cc779b336d846763074324
edgatcoder <edgar.gyurjyan@gmail.com>
2016-07-22 21:45:34 +0400

    Add files via upload

---
+++ edvik/edo/js1.js
@@ -0,0 +1,15 @@
+var hiddenDiv= document.getElementById("showAnswer");
+hiddenDiv.style.display = "none";
+var answer = document.getElementById("answer");
+
+
+
+var showAnswer=function(){
+	var password = document.getElementById("password").value;
+	answer.innerHTML = password;
+	document.getElementById("questionDiv").style.display = "none";
+	document.getElementById("login").style.display = "none";
+	hiddenDiv.style.display = "block";
+	
+}
+
